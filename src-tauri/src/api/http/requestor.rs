37b6824a081d2c96d336277e0d1a3f072ed733e0
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2022-12-25 01:37:54 +0900

    🔥 - Removed `RequestSythetic`.

--- src-tauri/src/api/http/requestor.rs
+++ src-tauri/src/api/http/requestor.rs
@@ -66,8 +66,6 @@ use super::{Response, http_client, Refresh};
 /// Implement an instruction that requests a structure or 
 /// other object to be implemented in the [`Request::request()`] method.
 /// 
-/// If you want to import values from outside, see `RequestSynthetic`.
-/// 
 /// Also, HttpRequest is often asynchronous, so we require [`async_trait`].
 #[async_trait::async_trait]
 pub trait Request: Sized + serde::ser::Serialize {
@@ -76,18 +74,6 @@ pub trait Request: Sized + serde::ser::Serialize {
     type Rejection: std::fmt::Debug;
 
     async fn request(self, client: &Self::Client) -> Result<Self::Response, Self::Rejection>;
-}
-
-#[deprecated]
-#[async_trait::async_trait]
-pub trait RequestSyntheticMix1: Sized + serde::ser::Serialize {
-    type Client;
-    type Response;
-    type Rejection: std::fmt::Debug;
-
-    type Mix1;
-    
-    async fn request(self, client: &Self::Client, outer_1: Self::Mix1) -> Result<Self::Response, Self::Rejection>;
 }
 
 /// This structure is used to execute the asynchronous function 
@@ -106,26 +92,6 @@ impl<T: Request<Client = reqwest::Client>> Requestor<T> {
 
     pub async fn execute(self) -> Result<Response<T::Response>, T::Rejection> {
         match self.0.request(http_client()).await {
-            Ok(res) => Ok(Response::new(res)),
-            Err(error) => {
-                tracing::error!("request was not processed correctly. {:?}", error);
-                Err(error)
-            },
-        }
-    }
-}
-
-#[deprecated]
-impl<T: RequestSyntheticMix1<Client = reqwest::Client>> Requestor<T> {
-    /// Initialization process used when requesting external values.
-    /// 
-    /// The basic behavior is the same as [`Requestor::new()`], but [`RequestSyntheticT1`] is required to be implemented.
-    pub fn synthetic(order: T) -> Self {
-        Self(order)
-    }
-
-    pub async fn execute_sythetic(self, outer: T::Mix1) -> Result<Response<T::Response>, T::Rejection> {
-        match self.0.request(http_client(), outer).await {
             Ok(res) => Ok(Response::new(res)),
             Err(error) => {
                 tracing::error!("request was not processed correctly. {:?}", error);
